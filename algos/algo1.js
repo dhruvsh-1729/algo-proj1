5d69bf9b5429f0be34dc25a67883238ac8c10da9
Dhruv Shah <dhruvshdarshansh@gmail.com>
2024-07-13 21:21:43 +0530

    added some date functionality

--- algos/algo1.js
+++ algos/algo1.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import axios from 'axios';
+import { format, parse, addHours, addMinutes } from 'date-fns';
 
 const stations = [
     { "zone_name": "Northern Railway", "zone": "NR", "station_name": "New Delhi", "station_code": "NDLS" },
@@ -117,19 +118,19 @@ const stations = [
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Bhubaneswar", "station_code": "BBS" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Puri", "station_code": "PURI" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Visakhapatnam", "station_code": "VSKP" },
-    { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Sambalpur", "station_code":"SBP" },
+    { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Sambalpur", "station_code": "SBP" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Khurda Road", "station_code": "KUR" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Cuttack", "station_code": "CTC" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Berhampur", "station_code": "BAM" },
     { "zone_name": "East Coast Railway", "zone": "ECoR", "station_name": "Rayagada", "station_code": "RGDA" }
-  ]
+]
 
 const router = new Router();
 
 const baseUrl = 'http://localhost:8000/trains/';
 
 const zones = [
-    'NR','CR','WR','NER','SR','ER','SER','SCR','SWR','NCR','NWR','SECR','NFR','ECR','ECOR','CSTM','NE'
+    'NR', 'CR', 'WR', 'NER', 'SR', 'ER', 'SER', 'SCR', 'SWR', 'NCR', 'NWR', 'SECR', 'NFR', 'ECR', 'ECOR', 'CSTM', 'NE'
 ]
 
 
@@ -139,58 +140,117 @@ router.get("/getZones", async (req, res) => {
     const src = req.query.src;
     const dest = req.query.dest;
 
-    const response1 = await axios.get(baseUrl+`stationLive?code=${src}`);
-    // console.log(response1.data.data[0]);
-   const filterSrcExpressTrains = response1.data.data.filter(train => Number(train.train_no)<20000 && Number(train.train_no)>=10000);
+    const response1 = await axios.get(baseUrl + `stationLive?code=${src}`);
+    //response 1 does not give the correct information for every station, for example Darjeeling it wont give the data
+    if (response1.data.data.length > 0) {
+        const filterSrcExpressTrains = response1.data.data;
 
-   let srcTrueZone, destTrueZone;
+        let srcTrueZone, destTrueZone;
 
-   for(const train of filterSrcExpressTrains){
-    const srcTrainRoute = (await axios.get(baseUrl+`getRoute?trainNo=${train.train_no}`)).data.data;
-    const srcZone = srcTrainRoute.filter(station => {
-     if(station.source_stn_code === src) return station;
-    })[0].zone;
-    console.log(srcZone);
-    if(zones.includes(srcZone)){
-        srcTrueZone = srcZone;
-        break;
+        for (const train of filterSrcExpressTrains) {
+            const srcTrainRoute = (await axios.get(baseUrl + `getRoute?trainNo=${train.train_no}`)).data.data;
+            const srcZone = srcTrainRoute.filter(station => {
+                if (station.source_stn_code === src) return station;
+            })[0].zone;
+            console.log(srcZone);
+            if (zones.includes(srcZone)) {
+                srcTrueZone = srcZone;
+                break;
+            }
+        }
+
+        const response2 = await axios.get(baseUrl + `stationLive?code=${dest}`);
+        const filterDestExpressTrains = response2.data.data;
+
+        for (const train of filterDestExpressTrains) {
+            const destTrainRoute = (await axios.get(baseUrl + `getRoute?trainNo=${train.train_no}`)).data.data;
+            const destZone = destTrainRoute.filter(station => {
+                if (station.source_stn_code === dest) return station;
+            })[0].zone;
+            console.log(destZone)
+            if (zones.includes(destZone)) {
+                destTrueZone = destZone;
+                break;
+            }
+        }
+
+        if (srcTrueZone === 'CSTM') srcTrueZone = 'CR';
+        else if (destTrueZone === 'CSTM') destTrueZone = 'CR';
+        if (zones.includes(srcTrueZone + 'R')) srcTrueZone = srcTrueZone + 'R'
+        if (zones.includes(destTrueZone + 'R')) destTrueZone = destTrueZone + 'R'
+
+        res.status(200).send({ data: { srcTrueZone, destTrueZone } })
     }
-   }
-
-   const response2 = await axios.get(baseUrl+`stationLive?code=${dest}`);
-   const filterDestExpressTrains = response2.data.data.filter(train => Number(train.train_no)<20000 && Number(train.train_no)>=10000);
-
-   for(const train of filterDestExpressTrains){
-    const destTrainRoute = (await axios.get(baseUrl+`getRoute?trainNo=${train.train_no}`)).data.data;
-    const destZone = destTrainRoute.filter(station => {
-     if(station.source_stn_code === dest) return station;
-    })[0].zone;
-    console.log(destZone)
-    if(zones.includes(destZone)){
-        destTrueZone = destZone;
-        break;
+    else {
+        //we will have to find a way for finding the zones or something else for these stations
+        res.status(200).send({ data: { srcTrueZone: "no train on this station found", destTrueZone: "no train on this station found" } })
     }
-   }
-
-  if(srcTrueZone==='CSTM') srcTrueZone='CR';
-  else if(destTrueZone==='CSTM') destTrueZone='CR';
-  if(zones.includes(srcTrueZone+'R')) srcTrueZone=srcTrueZone+'R'
-  if(zones.includes(destTrueZone+'R')) destTrueZone=destTrueZone+'R'
-
-   res.status(200).send({data:{srcTrueZone,destTrueZone}})
 });
-
 
 // To get all the stations from the source zone and the destination zone
 // and then get all the trains from the stations O(N*M)
 
-router.get('/getLongTrains', async (req,res)=>{
+// Auxiliary Functions
+function getNextDate(dateStr) {
+    // Split the input date string
+    const [day, month, year] = dateStr.split('-').map(Number);
+
+    // Create a new Date object (month is 0-based, so subtract 1)
+    const date = new Date(year, month - 1, day);
+
+    // Increment the date by one day
+    date.setDate(date.getDate() + 1);
+
+    // Extract the next day's day, month, and year
+    const nextDay = date.getDate();
+    const nextMonth = date.getMonth() + 1; // Months are 0-based, so add 1
+    const nextYear = date.getFullYear();
+
+    // Format the new date as DD-MM-YYYY, ensuring two digits for day and month
+    const formattedNextDay = nextDay.toString().padStart(2, '0');
+    const formattedNextMonth = nextMonth.toString().padStart(2, '0');
+
+    return `${formattedNextDay}-${formattedNextMonth}-${nextYear}`;
+}
+
+// Checks cmopatibility of the date and time
+function isCompatible(train1, train2) { // train1 is to reach a particular station, train 2 is to leave from that
+    if (train1.date === train2.date) {
+        return Number(train1.to_time) <= Number(train2.from_time);
+    }
+    else if (train2.date === getNextDate(train1.date)) {
+        return true;
+    }
+    return false;
+}
+
+// Get new date
+function addTimeToDate(dateStr, timeStr) {
+    // Parse the date from 'DD-MM-YYYY' format
+    const parsedDate = parse(dateStr, 'dd-MM-yyyy', new Date());
+
+    // Extract hours and minutes from the time string
+    const [hours, minutes] = timeStr.split('.').map(Number);
+
+    // Add hours and minutes to the parsed date
+    const dateWithAddedHours = addHours(parsedDate, hours);
+    const finalDate = addMinutes(dateWithAddedHours, minutes);
+
+    console.log({parsedDate, hours, minutes, dateWithAddedHours, finalDate});
+
+    // Format the final date as 'DD-MM-YYYY'
+    return format(finalDate, 'dd-MM-yyyy');
+}
+
+router.get('/getLongTrains', async (req, res) => {
     const src = req.query.src;
     const dest = req.query.dest;
     const date = req.query.date;
+    //DD-MM-YYYY
+    const nextDate = getNextDate(date);
 
     const response = await axios.get(`http://localhost:8000/algo1/getZones?src=${src}&dest=${dest}`);
-    const {srcTrueZone, destTrueZone} = response.data.data;
+    const { srcTrueZone, destTrueZone } = response.data.data;
 
     const srcImpStations = stations.filter(station => station.zone === srcTrueZone);
     const destImpStations = stations.filter(station => station.zone === destTrueZone);
@@ -199,24 +259,153 @@ router.get('/getLongTrains', async (req,res)=>{
 
     let allTrains = [];
 
-    const directTrainsAvailable = await axios.get(baseUrl+`getTrainOn?from=${src}&to=${dest}&date=${date}`);
-    const directTrains = directTrainsAvailable.data.data;
+    const directTrainsAvailable = await axios.get(baseUrl + `getTrainOn?from=${src}&to=${dest}&date=${date}`);
+    const directTrainsAvailableOnNextDate = await axios.get(baseUrl + `getTrainOn?from=${src}&to=${dest}&date=${nextDate}`);
 
-    console.log(directTrains);
+    if (directTrainsAvailable.data.data !== "No direct trains found") {
+        allTrains = [...directTrainsAvailable.data.data];
+    }
+    if (directTrainsAvailableOnNextDate.data.data !== "No direct trains found") {
+        allTrains = [...directTrainsAvailableOnNextDate.data.data];
+    }
 
-    allTrains = [...directTrains];
+    console.log(allTrains);
 
-    for(const srcStn of srcImpStations) {
-        for(const destStn of destImpStations) {
-            if(srcStn.station_code !== destStn.station_code){
-            const trainsAvailable = await axios.get(baseUrl+`getTrainOn?from=${srcStn.station_code}&to=${destStn.station_code}&date=${date}`);
-            const trains = trainsAvailable.data.data;
-            allTrains = [...trains,...allTrains];
+    if (allTrains.length > 0) {
+        return res.status(200).send({ data: allTrains, direct:true });
+    }
+
+    for (const srcStn of srcImpStations) {
+        for (const destStn of destImpStations) {
+            // console.log(srcStn, destStn)
+            if (srcStn.station_code !== destStn.station_code) {
+
+                const trainsAvailable = await axios.get(baseUrl + `getTrainOn?from=${srcStn.station_code}&to=${destStn.station_code}&date=${date}`);
+                const trainsAvailableOnNextDate = await axios.get(baseUrl + `getTrainOn?from=${srcStn.station_code}&to=${destStn.station_code}&date=${nextDate}`);
+                let trains = [];
+                if (trainsAvailable.data.data !== "No direct trains found") {
+                    trains = [...trainsAvailable.data.data];
+                }
+                if (trainsAvailableOnNextDate.data.data !== "No direct trains found") {
+                    trains = [...trainsAvailableOnNextDate.data.data];
+                }
+                allTrains = [...trains, ...allTrains];
             }
         }
     }
 
-    res.status(200).send({data:allTrains})
+    console.log("length:", allTrains.length);
+
+    return res.status(200).send({ data: allTrains , direct:false })
+})
+
+
+router.get('/actualTrains', async (req, res) => {
+    const src = req.query.src;
+    const dest = req.query.dest;
+    const date = req.query.date;
+    const nextDate = getNextDate(date);
+
+    //here, we get the long trains possible from the zones
+    const response = await axios.get(`http://localhost:8000/algo1/getLongTrains?src=${src}&dest=${dest}&date=${date}`);
+    const allTrains = response.data.data;
+    const directTrains = response.data.direct;
+
+    if(directTrains)
+    return res.status(200).send({ data: allTrains});
+
+    let train_ans = [];
+    let src_trains = [];
+
+    console.log({allTrains});
+
+    //we run the loop for all the long trains found to find the 
+    // trains from the actual source to source of long train and
+    // then from destination of long train to our actual destination
+
+    for (const long_train of allTrains) {
+        // for source
+        console.log({long_train});
+        const virtual_src = long_train.train_base.from_stn_code;
+        let src_train_list = []; //this list will store all the trains from the actual source to virtual source
+
+        console.log({virtual_src});
+
+        if (src !== virtual_src) {
+            //if the actual source is not equal to the virtual source 
+            // then we need to find the trains so we call the APIs on the same date as well as the next date
+            const src_train_date = await axios.get(baseUrl + `getTrainOn?from=${src}&to=${virtual_src}&date=${date}`);
+            const src_train_next_date = await axios.get(baseUrl + `getTrainOn?from=${src}&to=${virtual_src}&date=${nextDate}`);
+
+            console.log({src_train_date:src_train_date.data.data, src_train_next_date:src_train_next_date.data.data});
+
+            if (src_train_date.data.data !== "No direct trains found") {
+                src_train_list = [...src_train_list,...src_train_date.data.data];
+            }
+            if (src_train_next_date.data.data !== "No direct trains found") {
+                src_train_list = [...src_train_list,...src_train_next_date.data.data];
+            }
+
+            if(src_train_list.length > 0){
+                for (const src_train of src_train_list) {
+                    console.log({src_train});
+                    if (isCompatible(src_train, long_train)) {
+                        src_trains = [...src_trains , { src_train, long_train }];
+                    }
+                }
+            }
+            else{
+                continue;
+            }
+        }
+        else {
+            src_trains = [...src_trains, { src_train: "None", long_train }];
+        }
+
+
+        return res.status(200).send({data:src_trains});
+        console.log({src_trains});
+        // for destination
+
+        const virtual_dest = long_train.train_base.to_stn_code;
+        console.log({virtual_dest});
+        let dest_train_list = [];
+
+        // the start date of each dest trains is supposed to be differernt for each source train ....add a loop for that
+    
+        const new_date = addTimeToDate(date,long_train.train_base.travel_time);
+        console.log({new_date});
+        const new_next_date = getNextDate(new_date);
+
+        if (dest !== virtual_dest) {
+            const dest_train_date = await axios.get(baseUrl + `getTrainOn?from=${virtual_dest}&to=${dest}&date=${new_date}`);
+            const dest_train_next_date = await axios.get(baseUrl + `getTrainOn?from=${virtual_dest}&to=${dest}&date=${new_next_date}`);
+
+            console.log({dest_train_date:dest_train_date.data.data, dest_train_next_date:dest_train_next_date.data.data});
+
+            if (dest_train_date.data.data !== "No direct trains found") {
+                dest_train_list = [...dest_train_list,...dest_train_date.data.data];
+            }
+            if (dest_train_next_date.data.data !== "No direct trains found") {
+                dest_train_list = [...dest_train_list,...dest_train_next_date.data.data];
+            }
+
+            for (const dest_train of dest_train_list) {
+                console.log({dest_train});
+                if (isCompatible(long_train, dest_train)) {
+                    for(const total_train of src_trains){
+                        console.log({total_train});
+                        // check format of this list
+                        train_ans = [...train_ans, {...total_train, dest_train}];
+                    }
+                }
+            }
+        }
+    }
+
+    console.log({train_ans});
+
+    return res.status(200).json({train_ans})
 })
 
 
